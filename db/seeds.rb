ed1363b6ff801b783f460c64eb23c2261dac38c5
Nick Tassinari <ntass7800@gmail.com>
2023-05-23 18:53:56 -0600

    finished user story 14 approving and rejecting applications

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,11 +20,18 @@ d_zappa = shelter.pets.create!(adoptable: true, age: 2, breed: "mini poodle", na
 bowser = shelter.pets.create!(adoptable: true, age: 4, breed: "big chonk", name: "Bowser")
 l_bowser = shelter.pets.create!(adoptable: true, age: 2, breed: "lil chonk", name: "Little Bowser")
 
-shelter1 = Shelter.create!(foster_program: true, name: "Do Again Pets", city: "Barne", rank: 1)
-shelter2 = Shelter.create!(foster_program: true, name: "Rickys used pets", city: "Sunnyvale", rank: 1)
-shelter3 = Shelter.create!(foster_program: true, name: "Not Ricky's re-used pets", city: "Orange", rank: 1)
+@shelter1 = Shelter.create!(foster_program: true, name: "Do Again Pets", city: "Barne", rank: 1)
+@shelter2 = Shelter.create!(foster_program: true, name: "Rickys used pets", city: "Sunnyvale", rank: 1)
+@shelter3 = Shelter.create!(foster_program: true, name: "Not Ricky's re-used pets", city: "Orange", rank: 1)
 
 @joey = Application.create!(name: "Joey", address: "73 Shifty St", city: "Oakland", state: "MA", zip_code: "09342", description: "I'm a good people, totally not robot", status: "Pending" )
 @lars = Application.create!(name: "Lars Fredrickson", address: "73 Shifty St", city: "Oakland", state: "MA", zip_code: "09342", description: "I'm a good people, totally not robot", status: "Pending" )
 @matt = Application.create!(name: "Matt Freeman", address: "73 Shifty St", city: "Oakland", state: "MA", zip_code: "09342", description: "I'm a good people, totally not robot", status: "In Progress" )
 @tim = Application.create!(name: "Tim Armstrong", address: "73 Shifty St", city: "Oakland", state: "MA", zip_code: "09342", description: "Mohawk", status: "Rejected" )
+
+# @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
+#           @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
+#           @pet_3 = @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
+#           @pet_4 = @shelter_1.pets.create(name: "Ann", breed: "ragdoll", age: 5, adoptable: true)
+  
+@pet_app_1 = @joey.pets_applications.create!(pet_id: zappa.id, pet_status: zappa.adoptable)
--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "Admin Applications Show Page" do
         @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
         @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
 
-        @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: false)
+        @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
         @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
         @pet_3 = @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
         @pet_4 = @shelter_1.pets.create(name: "Ann", breed: "ragdoll", age: 5, adoptable: true)
@@ -35,7 +35,7 @@ RSpec.describe "Admin Applications Show Page" do
         @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
         @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
 
-        @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: false)
+        @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
         @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
         @pet_3 = @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
         @pet_4 = @shelter_1.pets.create(name: "Ann", breed: "ragdoll", age: 5, adoptable: true)
@@ -45,7 +45,7 @@ RSpec.describe "Admin Applications Show Page" do
         @tim = Application.create!(name: "Tim Armstrong", address: "73 Shifty St", city: "Oakland", state: "MA", zip_code: "09342", description: "Mohawk", status: "Rejected" )
 
         @pet_app_1 = @joey.pets_applications.create!(pet_id: @pet_1.id, pet_status: @pet_1.adoptable)
-        @pet_app_3 = @tim.pets_applications.create!(pet_id: @pet_2.id,pet_status: @pet_2.adoptable)
+        @pet_app_3 = @lars.pets_applications.create!(pet_id: @pet_2.id,pet_status: @pet_2.adoptable)
 
         visit "/admin/applications/#{@lars.id}"
         click_button "Reject"
@@ -53,6 +53,41 @@ RSpec.describe "Admin Applications Show Page" do
         expect(page).to_not have_content("Approve")
         expect(page).to_not have_content("Reject")
         expect(page).to have_content("Application Denied")
+      end
+
+        # User Story 14
+        # As a visitor
+        # When there are two applications in the system for the same pet
+        # When I visit the admin application show page for one of the applications
+        # And I approve or reject the pet for that application
+        # When I visit the other application's admin show page
+        # Then I do not see that the pet has been accepted or rejected for that application
+        # And instead I see buttons to approve or reject the pet for this specific application
+        # Completed Applications
+        # Once all pets on an application have been marked either accepted or rejected, then the application is no longer "Pending". If all the pets were accepted, then the application is "Accepted". If one or more pets on the application is rejected, then the entire application is "Rejected".
+
+        it "Won't allow a pet to be adopted by two seperate applications" do 
+          @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+          @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
+  
+          @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
+          @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
+          @pet_3 = @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
+          @pet_4 = @shelter_1.pets.create(name: "Ann", breed: "ragdoll", age: 5, adoptable: true)
+  
+          @joey = Application.create!(name: "Joey", address: "73 Shifty St", city: "Oakland", state: "MA", zip_code: "09342", description: "I'm a good people, totally not robot", status: "Pending" )
+          @lars = Application.create!(name: "Lars Fredrickson", address: "73 Shifty St", city: "Oakland", state: "MA", zip_code: "09342", description: "I'm a good people, totally not robot", status: "Pending" )
+          @tim = Application.create!(name: "Tim Armstrong", address: "73 Shifty St", city: "Oakland", state: "MA", zip_code: "09342", description: "Mohawk", status: "Rejected" )
+          @pet_app_1 = @joey.pets_applications.create!(pet_id: @pet_1.id, pet_status: @pet_1.adoptable)
+          @pet_app_3 = @lars.pets_applications.create!(pet_id: @pet_1.id, pet_status: @pet_1.adoptable)
+
+
+          visit "/admin/applications/#{@joey.id}"
+          click_button "Approve"
+          visit "/admin/applications/#{@lars.id}"
+
+          expect(page).to_not have_button("Approve")
+          expect(page).to_not have_button("Reject")
       end
     end
   end
